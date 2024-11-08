a3fbe12571a8fc92f9436522ecd3fbe561c86f93
smyooncochl <147586941+smyooncochl@users.noreply.github.com>
2024-11-08 15:27:07 +0900

    1.0.0 Release

--- samples/sample.py
+++ samples/sample.py
@@ -4,7 +4,7 @@ from cochl.sense import Result
 
 def main():
     api_config = sense.APIConfig(
-        speaker_recognition=True
+        speaker_recognition=False
     )
 
     client = sense.Client(
@@ -12,7 +12,7 @@ def main():
         api_config=api_config,
     )
 
-    result: Result = client.predict('sample_male_speech.wav')
+    result: Result = client.predict('sample_gunshot.wav')
 
     print(result.events.to_dict())
     print(result.events.to_summarized_result())
